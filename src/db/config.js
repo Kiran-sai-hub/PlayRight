287f688aa3454f3ab567e6c902e00fc06bf6d255
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2025-02-28 14:35:33 +0530

    Connection to mongo compass

---
+++ src/db/config.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+dotenv.config(); // configuring environement variables...
+
+const url = `${process.env.DB_URL}/${DB_NAME}`;
+
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect(url);
+        console.log("MongoDB Connected!!");
+    }catch(err){
+        console.log("MongoDB Connection Failed");
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+import connectDB from './db/config.js';
+
+dotenv.config({
+    path: './env',
+});
+connectDB();
