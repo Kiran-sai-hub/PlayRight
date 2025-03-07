401c3631b30506ff3a602aa48a913b53c7fed6d0
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2025-03-07 15:02:05 +0530

    minor bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async(localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: 'auto'
         });
-        console.log("File uploaded Successfully!", response.url);
+        // console.log("File uploaded Successfully!", response.url);
+        fs.unlinkSync(localFilePath);
         return response;
 
     }catch(err){
