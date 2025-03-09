aa16d22eb6da8b77eff1fd6ed091f6ff7f6b250e
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2025-03-10 02:41:17 +0530

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = Schema({
+    subscriber: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    channel: {
+        type: mongoose.Schema.Types.ObjectId, // Since a channel is also a user
+        ref: 'User',
+    }
+}, {timestamps: true});
+
+export const Subscription = mongoose.model('Subscription', subscriptionSchema);
