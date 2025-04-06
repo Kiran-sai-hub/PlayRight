cf973cc8b961f74b80e34493b07b9e959c2fbcc5
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2025-04-07 04:35:00 +0530

    added subscription aggregarion pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -311,6 +311,83 @@ const updateUserCoverImage = asyncHandler(async(req,res,next) => {
     );
 });
 
+const getUserChannelProfile = asyncHandler(async(req,res,next) => {
+    const {username} = req.params;
+
+    if(!username?.trim()){
+        throw new ApiError(400, "Username is missing");
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers",
+            },
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo",
+            },
+        },
+        {   
+            $addFields: {
+                subscriberCount: {
+                    $size: "$subscribers"
+                },
+                subscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false,
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                _id: 1,
+                fullname: 1,
+                username: 1,
+                subscriberCount: 1,
+                subscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+            }
+        }
+        
+    ]);
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exist");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            channel[0],
+            "User Channel fetched Successfully",
+        )
+    );
+});
+
 export {
     registerUser,
     loginUser,
@@ -321,4 +398,5 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
+    getUserChannelProfile,
 };
